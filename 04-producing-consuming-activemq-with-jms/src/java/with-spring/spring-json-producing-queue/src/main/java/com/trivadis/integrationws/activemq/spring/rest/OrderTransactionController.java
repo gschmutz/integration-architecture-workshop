2f25a7ba8bd786e19423e3e8d4e842fc725ceb25
Guido Schmutz <guido.schmutz@trivadis.com>
2018-06-26 13:02:00 +0200

    latest version

---
+++ 04-producing-consuming-activemq-with-jms/src/java/with-spring/spring-json-producing-queue/src/main/java/com/trivadis/integrationws/activemq/spring/rest/OrderTransactionController.java
@@ -0,0 +1,23 @@
+package com.trivadis.integrationws.activemq.spring.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import com.trivadis.integrationws.activemq.spring.documents.OrderTransaction;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/transaction")
+public class OrderTransactionController {
+
+  @Autowired private JmsTemplate jmsTemplate;
+
+  @PostMapping("/send")
+  public void send(@RequestBody OrderTransaction transaction) {
+    System.out.println("Sending a transaction....");
+    jmsTemplate.convertAndSend(
+        "OrderTransactionQueue", transaction);
+  }
+}
